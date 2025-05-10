6b396a3f7a7b79bbd4acadf14f0ff7c00e705f7d
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-05-10 12:30:22 +0200

    add object handle float

--- object/object.c
+++ object/object.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
 #include "snekobject.h"
 
+snek_object_t *new_snek_float(float value) {
+	snek_object_t *obj = malloc(sizeof(snek_object_t));
+	if (obj == NULL) {
+		return NULL;
+	}
+
+	obj->kind = FLOAT;
+	obj->data.v_float = value;
+	return obj;
+}
+
 snek_object_t *new_snek_integer(int value) {
 	snek_object_t *obj = malloc(sizeof(snek_object_t));
 	if (obj == NULL) {
